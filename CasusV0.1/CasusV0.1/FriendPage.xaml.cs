0fbe081683ea74d443233cc089029fc3789fa5e0
Nathan Groenveld <2062294groenveld@zuyd.nl>
2021-01-19 10:08:31 +0100

    Version 2

---
+++ CasusV0.1/CasusV0.1/FriendPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CasusV0._1
+{
+public partial class FriendPage : ContentPage
+{
+    public FriendPage()
+    {
+        InitializeComponent();
+    }
+}
+}
---
+++ CasusV0.1/CasusV0.1/ProfilePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CasusV0._1
+{
+public partial class ProfilePage : ContentPage
+{
+    public ProfilePage()
+    {
+        InitializeComponent();
+    }
+}
+}
